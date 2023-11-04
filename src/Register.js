68652b7c47bf66601e898ac35237e6ce65591a69
Altina Hasani <ah60108@ubt-uni.net>
2023-11-04 14:09:02 +0100

    some changes added on register functionality

--- src/Register.js
+++ src/Register.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 
 class RegistrationForm extends Component {
-    constructor() {
-      super();
-      this.state = {
-        username: '',
-        password: '',
-        email: '',
-        registration_date: new Date().toISOString(), // Initialize with the current date and time
-        role: 'user',
-        name: '',
-        specialization: '',
-        availability: '',
-        experience: '',
-        bio: '',
-       
-      };
-    }
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+      confirmPassword: '', // Added confirmPassword field
+      email: '',
+      role: 'user',
+      name: '',
+      specialization: '',
+      availability: '',
+      experience: '',
+      bio: '',
+    };
+  }
 
   handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,9 +25,55 @@ class RegistrationForm extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
 
-    // Perform form validation and submit the data to your Node.js server for registration
-    // You can use the 'fetch' API or a library like Axios to make a POST request to your server
-  }
+    // Add form validation logic here to check if the password and confirmPassword match
+
+    if (this.state.password !== this.state.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    // Prepare the registration data from the form state
+    const registrationData = {
+      username: this.state.username,
+      password: this.state.password,
+      email: this.state.email,
+      role: this.state.role,
+      name: this.state.name,
+      specialization: this.state.specialization,
+      availability: this.state.availability,
+      experience: this.state.experience,
+      bio: this.state.bio,
+    };
+
+    // Make an HTTP POST request to the server's registration API
+    fetch('http://localhost:3000/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(registrationData),
+      })
+        .then((response) => {
+          if (response.ok) {
+            // Registration was successful
+            response.json().then((data) => {
+              console.log('Registration successful:', data);
+              // Optionally redirect to a login page or show a success message
+            });
+          } else {
+            // Registration failed
+            response.json().then((data) => {
+              console.error('Registration failed:', data.message);
+              // You can show an error message to the user.
+            });
+          }
+        })
+        .catch((error) => {
+          console.error('Error registering:', error);
+          // Handle any unexpected errors here.
+        });
+    }
+    
 
   render() {
     return (
@@ -46,9 +91,8 @@ class RegistrationForm extends Component {
           </label>
           <label>
             Confirm Password:
-            <input type="password" name="Confirm Password" value={this.state.password} onChange={this.handleInputChange} required />
+            <input type="password" name="confirmPassword" value={this.state.confirmPassword} onChange={this.handleInputChange} required />
           </label>
-
 
           <label>
             Email:
@@ -75,31 +119,36 @@ class RegistrationForm extends Component {
                 <input type="text" name="specialization" value={this.state.specialization} onChange={this.handleInputChange} />
               </label>
               <label>
-  Availability (Therapist):
-  <input
-    type="text"
-    name="availability"
-    value={this.state.availability}
-    onChange={this.handleInputChange}
-    placeholder="e.g., Monday-Friday, 9 AM - 5 PM"
-  />
-</label>
+                Availability (Therapist):
+                <input
+                  type="text"
+                  name="availability"
+                  value={this.state.availability}
+                  onChange={this.handleInputChange}
+                  placeholder="e.g., Monday-Friday, 9 AM - 5 PM"
+                />
+              </label>
 
-<label>
-  Experience (Therapist):
-  <input
-    type="number" name="experience" value={this.state.experience} onChange={this.handleInputChange} placeholder="e.g., 5 years"
- />
-</label>
+              <label>
+                Experience (Therapist):
+                <input
+                  type="number"
+                  name="experience"
+                  value={this.state.experience}
+                  onChange={this.handleInputChange}
+                  placeholder="e.g., 5 years"
+                />
+              </label>
 
-<label>
-  Bio (Therapist):
-  <textarea
-    name="bio" value={this.state.bio} onChange={this.handleInputChange} placeholder="Tell us about yourself and your experience as a therapist."
-  ></textarea>
-</label>
-
-              
+              <label>
+                Bio (Therapist):
+                <textarea
+                  name="bio"
+                  value={this.state.bio}
+                  onChange={this.handleInputChange}
+                  placeholder="Tell us about yourself and your experience as a therapist."
+                ></textarea>
+              </label>
             </>
           )}
 
@@ -108,6 +157,6 @@ class RegistrationForm extends Component {
       </div>
     );
   }
-}
 
+}
 export default RegistrationForm;
--- src/back/index.js
+++ src/back/index.js
@@ -1,28 +1,48 @@
-const express=require("express");
-const app=express();
-const mysql=require("mysql");
-const bodyParser=require("body-parser");
-const cors=require("cors");
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const bcrypt = require('bcrypt'); // Add this for password hashing
 
-const db=mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"password",
-    database:"tranquillo",
+app.use(cors());
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "tranquillo",
 });
-db.getConnection((err,connection)=>{
-    if(err){
-        console.error("Error connecting to MYSQL server:",err);
-    }else{
-        console.log("Connected to MYSQL server.");
-    }
-    connection.release();
-})
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to MYSQL server:", err);
+  } else {
+    console.log("Connected to MYSQL server.");
+  }
+  connection.release();
+});
+
+app.use(bodyParser.json()); // Use body-parser middleware to parse JSON data
+
 app.post('/login', (req, res) => {
-    // Check user credentials (e.g., username and password)
-    // Query the database to get the user's role
-  
-    // Determine the user's role
+  const { username, password } = req.body;
+
+  // Check user credentials and get the user's role from the database
+  const sql = "SELECT role FROM Users WHERE username = ?";
+  db.query(sql, [username], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ message: "Server error" });
+    }
+    if (result.length === 0) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const userRole = result[0].role;
+
+    // Create a session or token based on the user's role
+    // Redirect to the appropriate dashboard
     if (userRole === 'admin') {
       // Create a session or token with 'admin' role
       // Redirect to the admin dashboard
@@ -37,4 +57,55 @@ app.post('/login', (req, res) => {
       // Redirect to the user dashboard
     }
   });
+});
+
+app.post('/api/register', async (req, res) => {
+    console.log(req.body); 
   
+    try {
+      const {
+        username,
+        password,
+        email,
+        role,
+        name,
+        specialization,
+        availability,
+        experience,
+        bio,
+      } = req.body;
+  
+      // Validate input data (e.g., check if required fields are present)
+  
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      let sql, values;
+  
+      if (role === 'user') {
+        sql = 'INSERT INTO users (username, password, email, role) VALUES (?, ?, ?, ?)';
+        values = [username, hashedPassword, email, role];
+      } else if (role === 'therapist') {
+        sql = 'INSERT INTO therapists (name, specialization, availability, experience, bio) VALUES (?, ?, ?, ?, ?)';
+        values = [name, specialization, availability, experience, bio];
+      } else {
+        return res.status(400).json({ message: 'Invalid role' });
+      }
+  
+      db.query(sql, values, (error, result) => {
+        if (error) {
+          console.error(error);
+          return res.status(500).json({ message: 'Registration failed' });
+        }
+        return res.status(200).json({ message: 'Registration successful' });
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Registration failed' });
+    }
+  });
+  
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
